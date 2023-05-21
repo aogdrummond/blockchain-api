119ce5d0f56d22ea1a602c07bbda1bb2b7489953
aogdrummond <alexandre@olga.tech>
2023-05-21 19:54:10 -0300

    .

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,11 @@
-from controller import generate_address_to_crypto, list_addresses, retrieve_address, persist_address
+import os
+from controller import  list_addresses, retrieve_address, persist_address
 import json
 
 def lambda_handler(event, context):
 
-    data = json.loads(event["body"])
-    crypto_currency = data.get("crypto_currency")
-    if crypto_currency:
-        address = generate_address_to_crypto(crypto_currency)
-        persist_address(address, crypto_currency)
-        
-        return {
-        'statusCode': 201,
-        'body': json.dumps({"address": address})
-    }
-    else:
-        return {
-            'statusCode': 400,
-            'body': json.dumps('Hello from Lambda!')
-        }
-
+    addresses = list_addresses()
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"addresses": addresses})}
+    
